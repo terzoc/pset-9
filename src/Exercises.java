8f9ed7763cb2cb7c3e7b3fc933d46724db5ddc2d
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2020-01-30 09:03:59 -0500

    commonEnd done

--- src/Exercises.java
+++ src/Exercises.java
@@ -6,63 +6,65 @@ public class Exercises {
 		if (a == null || a.isEmpty() || b == null || b.isEmpty()) {
 			return false;
 		}
-		
-		// write your code here
-		
+
+		if (a.get(0) == b.get(0) || a.get(a.size() - 1) == b.get(b.size() - 1)) {
+			return true;
+		}
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public ArrayList<String> endsMeet(ArrayList<String> values, int n) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
-	
+
 	public int difference(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public double biggest(ArrayList<Double> numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public ArrayList<String> middle(ArrayList<String> values) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean everywhere(ArrayList<Integer> numbers, int x) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean consecutive(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean balance(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public int clumps(ArrayList<String> values) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
 }
